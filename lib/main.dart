f906a589aeaf1de0f9727110f54f181b3d9d7879
Saurabhgit157 <sy2961327@gmail.com>
2026-06-01 02:16:00 +0530

    wrapped app with changeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mealrecipeapp/app.dart';
+import 'package:provider/provider.dart';
+import 'package:mealrecipeapp/favorites_providers.dart';
 
 void main() {
-  runApp(const MealRecipeApp());
+  runApp(
+    // 1. Humne poori app ko Provider se lapetna
+    ChangeNotifierProvider(
+      //caunsa brain provider use krna h
+      create: (context) => FavouritesProvider(),
+      child: const MealRecipeApp(),
+    )
+  );
 }
 
